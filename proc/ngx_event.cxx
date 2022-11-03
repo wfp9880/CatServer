10a7579a1e1c04095d59f25c536d0e88b07308ea
wfp <1763898311@qq.com>
2022-11-03 20:26:53 +0800

    C++Linux服务器

---
+++ proc/ngx_event.cxx
@@ -0,0 +1,24 @@
+﻿//和开启子进程相关
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>   //信号相关头文件 
+#include <errno.h>    //errno
+#include <unistd.h>
+
+#include "ngx_func.h"
+#include "ngx_macro.h"
+#include "ngx_c_conf.h"
+
+//处理网络事件和定时器事件
+void ngx_process_events_and_timers()
+{
+    g_socket.ngx_epoll_process_events(-1); //-1表示卡着等待吧
+
+    //统计信息打印，考虑到测试的时候总会收到各种数据信息，所以上边的函数调用一般都不会卡住等待收数据
+    g_socket.printTDInfo();
+    
+    //...再完善
+}
+
